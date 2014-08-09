b9c307298cae9f46c0faacc8455fee0b8972435a
Blade Nelson <powderblock@gmail.com>
2014-08-09 01:32:53 -0400

    Removed Un-Needed RSS Loading Before While Loop
    Removed loading the RSS feeds at start, added comments.

--- RedAlert.py
+++ RedAlert.py
@@ -1,17 +1,19 @@
 import feedparser, time
 
-redditRSS = feedparser.parse('http://www.reddit.com/r/photoshopbattles/new/.rss')
-
-post = "New post. "+redditRSS['entries'][0]['title']+redditRSS.entries[0].published
+#Main loop.
 while(1):
-    time.sleep(30)
+    #RSS feed to parse.
     redditRSS = feedparser.parse('http://www.reddit.com/r/photoshopbattles/new/.rss')
 
+    #Make a string out of some of the items in the RSS feed.
     newPost = "New post. "+redditRSS['entries'][0]['title']+redditRSS.entries[0].published
 
+    #Check if the post has changed since last loop.
     if post != newPost:
         print("New post!")
         post = newPost
 
     else:
         print("No new post.")
+
+    time.sleep(30)
