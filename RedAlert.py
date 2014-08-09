953d6938d56e190ce16f1ce572a86af519ab9eda
Blade Nelson <powderblock@gmail.com>
2014-08-09 01:30:19 -0400

    Added Checking for New Posts
    Script now checks every x seconds for new posts, if new post, prints
    "New post!" If not, prints "No new post."

--- RedAlert.py
+++ RedAlert.py
@@ -1,5 +1,17 @@
-import feedparser
+import feedparser, time
 
 redditRSS = feedparser.parse('http://www.reddit.com/r/photoshopbattles/new/.rss')
 
-print("New post. "+redditRSS['entries'][0]['title'])
+post = "New post. "+redditRSS['entries'][0]['title']+redditRSS.entries[0].published
+while(1):
+    time.sleep(30)
+    redditRSS = feedparser.parse('http://www.reddit.com/r/photoshopbattles/new/.rss')
+
+    newPost = "New post. "+redditRSS['entries'][0]['title']+redditRSS.entries[0].published
+
+    if post != newPost:
+        print("New post!")
+        post = newPost
+
+    else:
+        print("No new post.")
