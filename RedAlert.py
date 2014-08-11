1a33c890f6cf548980edb8fa59fa06465f8edc07
Blade Nelson <powderblock@gmail.com>
2014-08-11 16:34:04 -0400

    Added updateRSS function, removed old RSS methods
    Added the function updateRSS, pulls the new RSS information, then puts
    different values in different variables (date, title, etc), removed old
    methods for obtaining RSS data. Also added "Successfully logged in."
    debug message. Made new variable called mail, holds number+'@'+gateway,
    avoids problems with server.sendmail due to string issues.

--- RedAlert.py
+++ RedAlert.py
@@ -6,6 +6,8 @@ server.starttls()
 #Get user information from user:
 account = str(raw_input("Enter Google Talk account username: (Example: username123) "))
 password = str(raw_input("Enter Google Talk account password: (Example: abc123) "))
+print(">Successfully logged in.")
+server.login(account+'@gmail.com', password)
 
 #Get number to text:
 number = str(raw_input("Enter a number to text. (Example: 1234567890) "))
@@ -13,25 +15,34 @@ number = str(raw_input("Enter a number to text. (Example: 1234567890) "))
 #Get SMS gateway:
 gateway = str(raw_input("Enter SMS gateway domain. (AT&T: txt.att.net, T-Mobile: tmomail.net) "))
 
-redditRSS = feedparser.parse('http://www.reddit.com/r/photoshopbattles/new/.rss')
-post = "New post. "+redditRSS['entries'][0]['title']+' Time and Date posted: '+redditRSS.entries[0].published
+mail = number+'@'+gateway
+
+def updateRSS():
+    global redditRSS, date, title
+    redditRSS = feedparser.parse('http://www.reddit.com/r/photoshopbattles/new/.rss')
+    title = redditRSS['entries'][0]['title']
+    date = redditRSS.entries[0].published
+
+updateRSS()
+post = title+' Time and Date posted: '+date
+
 
 #Main loop.
 while(1):
     server.login(account+'@gmail.com', password)
     print(">Loading RSS feed to parse.")
     #RSS feed to parse.
-    redditRSS = feedparser.parse('http://www.reddit.com/r/photoshopbattles/new/.rss')
+    updateRSS()
 
     #Make a string out of some of the items in the RSS feed.
-    newPost = "New post. "+redditRSS['entries'][0]['title']+' Time and Date posted: '+redditRSS.entries[0].published
+    newPost = title+' Time and Date posted: '+date
 
     #Check if the post has changed since last loop.
     if post != newPost:
         print("New post!\n")
         print newPost
         #Send alert to phone.
-        server.sendmail('', number+'@'+gateway, newPost)
+        server.sendmail('', mail, newPost)
         post = newPost
 
     else:
